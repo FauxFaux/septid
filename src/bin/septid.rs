55cb0bd042e52fe3d62ff8df6218a52b7291001b
Chris West (Faux) <git@goeswhere.com>
2020-05-30 21:41:02 +0100

    feat: switch to clap 3

--- src/bin/septid.rs
+++ src/bin/septid.rs
@@ -1,80 +1,62 @@
-use std::env;
 use std::fs;
+use std::path::PathBuf;
 
 use anyhow::anyhow;
 use anyhow::Context as _;
 use anyhow::Result;
 use async_std::task;
+use clap::Clap;
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
     task::block_on(run())
 }
 
+#[derive(Clap)]
+enum EncDec {
+    Encrypt,
+    Decryp,
+}
+
+#[derive(Clap)]
+#[clap(author, about, version, group = clap::ArgGroup::new("mode").required(true))]
+struct Opts {
+    /// forward data over an encrypted connection
+    #[clap(short, long, group = "mode", conflicts_with = "decrypt")]
+    encrypt: bool,
+    /// decrypt data from a encrypt, and forward it
+    #[clap(short, long, group = "mode")]
+    #[allow(dead_code)]
+    decrypt: bool,
+    /// key for encryption and authentication
+    #[clap(short, long, value_name = "FILE")]
+    key_file: PathBuf,
+    /// listen for connections
+    #[clap(short, long, value_name = "IP:PORT")]
+    source: String,
+    /// make connections to
+    #[clap(short, long, value_name = "HOST:PORT")]
+    target: String,
+    /// drop privileges after binding
+    #[clap(short, long, value_name = "USER:GROUP")]
+    uid: Option<String>,
+}
+
 async fn run() -> Result<()> {
-    let mut args = env::args();
-    let _us = args.next().unwrap_or_else(String::new);
-    let mut opts = getopts::Options::new();
-    opts.optflag("e", "encrypt", "forward data over an encrypted connection");
-    opts.optflag(
-        "d",
-        "decrypt",
-        "decrypt data from a encrypt, and forward it",
-    );
-
-    opts.reqopt(
-        "k",
-        "key-file",
-        "key for encryption and authentication",
-        "FILE",
-    );
-
-    opts.reqopt("s", "source", "listen for connections", "IP:PORT");
-    opts.reqopt("t", "target", "make connections to", "HOST:PORT");
-
-    opts.optopt("u", "uid", "drop privileges after binding", "USER:GROUP");
-
-    opts.optflag("h", "help", "print this help menu");
-
-    let matches = match opts.parse(args) {
-        Ok(matches) => matches,
-        Err(f) => {
-            eprintln!("error: {}", f);
-            return Ok(());
-        }
-    };
-
-    // TODO: error feedback
-    assert_eq!(
-        0,
-        matches.free.len(),
-        "all arguments must start with a hyphen"
-    );
-
-    let decrypt = matches.opt_present("d");
-    let encrypt = matches.opt_present("e");
-
-    assert_ne!(decrypt, encrypt, "-d or -e is required");
-
-    let addr = matches.opt_get::<String>("s")?.expect("opt required");
-
-    let target = matches.opt_get("t")?.expect("opt required");
-
-    let key_path: String = matches.opt_get("k")?.expect("opt required");
-
-    assert!(!matches.opt_present("u"), "-u unsupported");
+    let matches: Opts = Opts::parse();
+    assert!(matches.uid.is_none(), "-u unsupported");
 
     let key = {
-        let file =
-            fs::File::open(&key_path).with_context(|| anyhow!("opening key {:?}", key_path))?;
+        let file = fs::File::open(&matches.key_file)
+            .with_context(|| anyhow!("opening key {:?}", matches.key_file))?;
         septid::MasterKey::from_reader(file)?
     };
 
     let command = septid::server::start_server(&septid::server::StartServer {
-        bind_address: vec![addr],
-        encrypt,
+        bind_address: vec![matches.source],
+        encrypt: matches.encrypt,
         key,
-        target_address: vec![target],
+        target_address: vec![matches.target],
     })
     .await?;
 
